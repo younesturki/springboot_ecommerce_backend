db8f3855313595766b89ea5c092a63c947bdf5f9
younes.turki <younesturki@live.fr>
2020-12-02 16:49:20 +0100

    Creating Class for Country and State / Making refactoring

--- spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/dao/ProductRepository.java
@@ -12,4 +12,7 @@ public interface ProductRepository extends JpaRepository<Product,Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 
+    Page<Product> findByNameContaining(@RequestParam("name") String name, Pageable pageable);
+
+
 }
